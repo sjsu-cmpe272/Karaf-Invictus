197a08b1ba2a3dcd509f81edbedaf87a032facb0
anvitsaxena <anvit.saxena@sjsu.edu>
2016-10-02 11:21:14 -0700

    Add files via upload

---
+++ Twitter/AuthorizationKeys.php
@@ -0,0 +1,11 @@
+<?php 
+
+$settings = array(
+'oauth_access_token' => "Your oauth_access_token",
+'oauth_access_token_secret' => "Your oauth_access_token_secret",
+'consumer_key' => "Your consumer_key",
+'consumer_secret' => "Your consumer_secret"
+);
+
+?>
+//Keys have been removed due to security purposes.
